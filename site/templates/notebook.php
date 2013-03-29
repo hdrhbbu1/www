556a583cb211becc0a395247a56f9b36218d0f5a
Nicholas Young <nicholas@nicholaswyoung.com>
2013-03-29 15:47:55 -0500

    more changes

--- site/templates/notebook.php
+++ site/templates/notebook.php
@@ -1,10 +1,11 @@
 <?php snippet('header') ?>
 <section class='content notebook'>
-	<?php foreach($page->children()->visible()->flip() as $entry): ?>
+	<?php $entries = $page->children()->visible()->flip()->paginate(2) ?>
+	<?php foreach($entries as $entry): ?>
 	<article<?php ecco($entry->class(), " class='{$entry->class()}'") ?>>
 		<div class='metadata'>
-			<span class='date'><h2>Posted on:</h2></span>
-			<date><?php echo $entry->date('M d, Y') ?></date>
+			<span class='date'>Published on:</span>
+			<date><?php echo $entry->date('F d, Y') ?></date>
 		</div>
 		<div class='content'>
 			<a href='<?php echo $entry->url(); ?>'>
@@ -17,5 +18,25 @@
 		</div>
 	</article>
 	<?php endforeach ?>
+	<?php if ($entries->pagination()->hasPages()): ?>
+		<nav class='pagination'>
+			<ul>
+				<?php if ($entries->pagination()->hasPrevPage()): ?>
+					<li class='prev'>
+						<a class='prev' href='<?php echo $entries->pagination()->prevPageURL() ?>'>
+							&laquo; Previous
+						</a>
+					</li>
+				<?php endif ?>
+				<?php if ($entries->pagination()->hasNextPage()): ?>
+					<li class='next'>
+						<a class='next' href='<?php echo $entries->pagination()->nextPageURL() ?>'>
+							Next &raquo;
+						</a>
+					</li>
+				<?php endif ?>
+			</ul>
+		</nav>
+	<?php endif ?>
 </section>
 <?php snippet('footer') ?>

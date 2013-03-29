3c1c39f66241566070b68325857203955f3ddb03
Nicholas Young <nicholas@nicholaswyoung.com>
2013-03-29 00:02:19 -0500

    Moving on from the Slider.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,14 +1,11 @@
 <?php snippet('head') ?>
 <header class='top<?php ecco($page->class(), " {$page->class()}") ?>'>
-	<?php if ($page->url() == url()) : snippet('slideshow'); endif; ?>
-	<div class='navigation'>
-		<nav class='main'>
-			<a class='logo' href='<?php echo url() ?>'>
-			<h1><?php echo $site->title() ?></h1>
-			</a>
-			<?php snippet('main-menu') ?>
-		</nav>
-	</div>
+	<nav class='main'>
+		<a class='logo' href='<?php echo url() ?>'>
+		<h1><?php echo $site->title() ?></h1>
+		</a>
+		<?php snippet('main-menu') ?>
+	</nav>
 	<?php if (!$page->url() != url()): ?>
 		<?php snippet('heading') ?>
 	<?php endif ?>
--- site/snippets/slideshow.php
+++
@@ -1,8 +0,0 @@
-<div class='slides'>
-	<div class='slide' id='slide-01'>
-		<h2 class='billboard'>Photographer</h2>
-	</div>
-	<div class='slide' id='slide-02'>
-		<h2 class='billboard'>Filmmaker</h2>
-	</div>
-</div>

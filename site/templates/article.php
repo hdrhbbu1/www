8446217f5fc5fccadc1e86a36be205e6a8eb8af8
Nicholas Young <nicholas@nicholaswyoung.com>
2013-03-29 18:08:48 -0500

    Add article snippet.

--- site/templates/article.php
+++ site/templates/article.php
@@ -1,13 +1,5 @@
 <?php snippet('header') ?>
 <section class='content notebook'>
-	<article<?php ecco($page->class(), " class='{$page->class()}'") ?>>
-		<div class='metadata'>
-			<span class='date'><h2>Posted on:</h2></span>
-			<date><?php echo $page->date('M d, Y') ?></date>
-		</div>
-		<div class='content'>
-			<?php echo kirbytext($page->text()) ?>
-		</div>
-	</article>
+	<?php snippet('article', array('entry' => $page, 'context' => 'single')) ?>
 </section>
 <?php snippet('footer') ?>
--- site/templates/notebook.php
+++ site/templates/notebook.php
@@ -2,41 +2,8 @@
 <section class='content notebook'>
 	<?php $entries = $page->children()->visible()->flip()->paginate(2) ?>
 	<?php foreach($entries as $entry): ?>
-	<article<?php ecco($entry->class(), " class='{$entry->class()}'") ?>>
-		<div class='metadata'>
-			<span class='date'>Published on:</span>
-			<date><?php echo $entry->date('F d, Y') ?></date>
-		</div>
-		<div class='content'>
-			<a href='<?php echo $entry->url(); ?>'>
-				<h2><?php echo $entry->title() ?></h2>
-			</a>
-			<?php echo kirbytext(excerpt($entry->text(), 800)); ?>
-			<span class='continue'>
-				<a href='<?php echo $entry->url() ?>'>Continue reading...</a>
-			</span>
-		</div>
-	</article>
+		<?php snippet('article', array('entry' => $entry, 'context' => 'list')) ?>
 	<?php endforeach ?>
-	<?php if ($entries->pagination()->hasPages()): ?>
-		<nav class='pagination'>
-			<ul>
-				<?php if ($entries->pagination()->hasPrevPage()): ?>
-					<li class='prev'>
-						<a class='prev' href='<?php echo $entries->pagination()->prevPageURL() ?>'>
-							&laquo; Previous
-						</a>
-					</li>
-				<?php endif ?>
-				<?php if ($entries->pagination()->hasNextPage()): ?>
-					<li class='next'>
-						<a class='next' href='<?php echo $entries->pagination()->nextPageURL() ?>'>
-							Next &raquo;
-						</a>
-					</li>
-				<?php endif ?>
-			</ul>
-		</nav>
-	<?php endif ?>
+	<?php snippet('pagination', array('entries' => $entries)) ?>
 </section>
 <?php snippet('footer') ?>

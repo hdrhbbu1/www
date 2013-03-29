6dddbd4ee2c4f44f4f88d002f7869580ad9bffe7
Nicholas Young <nicholas@nicholaswyoung.com>
2013-03-28 19:42:48 -0500

    Add Viewport.

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -11,6 +11,7 @@
 		</title>
 	<?php endif; ?>
 	<meta charset='utf-8' />
+	<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
   <meta name='description' content='<?php echo html($site->description()) ?>' />
   <meta name='keywords' content='<?php echo html($site->keywords()) ?>' />
   <meta name='robots' content='index, follow' />

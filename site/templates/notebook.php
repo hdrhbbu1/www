8a8a155421fb26d5536d29113c53240e7ac7c580
Nicholas Young <nicholas@nicholaswyoung.com>
2013-03-30 00:51:29 -0500

    Tweak pagination.

--- site/templates/notebook.php
+++ site/templates/notebook.php
@@ -1,6 +1,6 @@
 <?php snippet('header') ?>
 <section class='content notebook'>
-	<?php $entries = $page->children()->visible()->flip()->paginate(2) ?>
+	<?php $entries = $page->children()->visible()->flip()->paginate(4) ?>
 	<?php foreach($entries as $entry): ?>
 		<?php snippet('article', array('entry' => $entry, 'context' => 'list')) ?>
 	<?php endforeach ?>

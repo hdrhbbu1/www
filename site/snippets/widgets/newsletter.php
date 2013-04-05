eb43c169947014ec034de3a556816c1d3d7b9eee
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-05 16:43:45 -0500

    Tweak

---
+++ site/snippets/widgets/newsletter.php
@@ -0,0 +1,10 @@
+<div class='widget newsletter'>
+	<h2 class='subhead'>Email Newsletter</h2>
+	<p>
+		Receive my monthly <em>Creative Digest</em> in your inbox, featuring articles, and tips on increasing your creative output.
+	</p>
+	<form class='madmimi' action='https://madmimi.com/signups/subscribe/<?php echo $list_id ?>' method='post' id='mad_mimi_signup_form' target='_blank'> 
+		<input id='signup_email' name='signup[email]' type='email' placeholder='you@example.com' />
+		<button>Sign up!</button>
+	</form> 
+</div>

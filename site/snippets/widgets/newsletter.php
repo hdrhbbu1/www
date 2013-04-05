227cbeae4527a4ae524ba3d20f08c88f7678f8be
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-05 17:10:12 -0500

    add name field

--- site/snippets/widgets/newsletter.php
+++ site/snippets/widgets/newsletter.php
@@ -4,7 +4,8 @@
 		Receive my monthly <em>Creative Digest</em> in your inbox, featuring articles, and tips on increasing your creative output.
 	</p>
 	<form class='madmimi' action='https://madmimi.com/signups/subscribe/<?php echo $list_id ?>' method='post' id='mad_mimi_signup_form' target='_blank'> 
-		<input id='signup_email' name='signup[email]' type='email' placeholder='you@example.com' />
+		<input id='madmimi_signup_name' name='signup[name]' type='text' placeholder='your name' />
+		<input id='madmimi_signup_email' name='signup[email]' type='email' placeholder='you@example.com' />
 		<button>Sign up!</button>
 	</form> 
 </div>

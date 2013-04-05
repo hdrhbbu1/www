f3fc1c57cab151e917cf798959df53a0b71e152d
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-05 17:09:02 -0500

    add list_id

--- site/templates/article.php
+++ site/templates/article.php
@@ -3,6 +3,6 @@
 	<?php snippet('article', array('entry' => $page, 'context' => 'single')) ?>
 </section>
 <aside class='sidebar'>
-	<?php snippet('widgets/newsletter') ?>
+	<?php snippet('widgets/newsletter', array('list_id' => 43842)) ?>
 </aside>
 <?php snippet('footer') ?>

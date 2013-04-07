b83ad6048388ef88e87c0819b39ab3befed0cbe2
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-06 21:59:27 -0500

    More meta tag tweaks.

--- site/snippets/head/meta.php
+++ site/snippets/head/meta.php
@@ -1,5 +1,13 @@
 <meta charset='utf-8' />
 <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
+<link rel='canonical' href='<?php echo $page->url() ?>' />
+<link rel='shortlink' href='<?php echo $page->tinyurl() ?>' />
+<link type='text/plain' rel='author' href='<?php echo url() . 'humans.txt' ?>' />
+<meta name='robots' content='index, follow' />
+<meta property='og:site_name' content='<?php echo $site->title() ?>' />
+<meta property='og:type' content='website' />
+<meta property='og:title' content='<?php echo $page->title() ?>' />
+<meta property='og:url' content='<?php echo $page->url() ?>' />
 <?php if ($page->keywords()): ?>
 	<meta name='keywords' content='<?php echo $page->keywords() ?>' />
 <?php else: ?>
@@ -7,14 +15,8 @@
 <?php endif ?>
 <?php if ($page->description()): ?>
 	<meta name='description' content="<?php echo $page->description() ?>" />
+<?php elseif ($page->subtitle()): ?>
+	<meta name='description' content="<?php echo $site->subtitle() ?>" />
 <?php else: ?>
 	<meta name='description' content="<?php echo $site->description() ?>" />
 <?php endif ?>
-<link rel='canonical' href='<?php echo $page->url() ?>' />
-<link rel='shortlink' href='<?php echo $page->tinyurl() ?>' />
-<link type='text/plain' rel='author' href='<?php echo url() . 'humans.txt' ?>' />
-<meta property='og:site_name' content='<?php echo $site->title() ?>' />
-<meta property='og:title' content='<?php echo $page->title() ?>' />
-<meta property='og:type' content='website' />
-<meta property='og:url' content='<?php echo $page->url() ?>' />
-<meta name='robots' content='index, follow' />

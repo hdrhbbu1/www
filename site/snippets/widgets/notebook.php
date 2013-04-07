401bb6188f989793910df94eed5ad11cf3ba9fb4
Nicholas Young <nicholas@nicholaswyoung.com>
2013-04-07 17:47:08 -0500

    Reworked article layout.

--- site/snippets/widgets/notebook.php
+++ site/snippets/widgets/notebook.php
@@ -7,7 +7,7 @@
 	<?php $entries = $pages->findByUid('notebook')->children()->visible()->flip()->limit(3) ?>
 	<ul class='basic-list'>
 	<?php foreach($entries as $entry): ?>
-		<?php if (isset($current) && $current->url() == $entry->uri()) continue ?>
+		<?php if (isset($current) && $current->uri() == $entry->uri()) continue ?>
 		<li>
 			<a href='<?php echo $entry->url() ?>'><h3><?php echo $entry->title() ?></h3></a>
 			<div class='meta'>

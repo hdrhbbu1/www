daeced09049abbe439fa7715f28adcfe381e1fe0
Nicholas Young <nicholas@nicholaswyoung.com>
2013-05-02 14:33:05 -0500

    tweaking

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,10 @@
 // @codekit-prepend jquery.js
 
+window.addEventListener('load', function () {
+	setTimeout(function () {
+		window.scrollTo(0, 1);
+	}, 0);
+});
+
 $(document).ready(function () {
 });
--- site/snippets/head/js.php
+++ site/snippets/head/js.php
@@ -1,4 +1,4 @@
-<?php echo js('assets/compiled/site.js') ?>
+<?php echo js('assets/js/site-ck.js') ?>
 <?php if ($page->files()->findByExtension('js')): ?>
 	<?php foreach ($page->files()->findByExtension('js') as $js): ?>
 		<?php echo js($js->url()) ?>

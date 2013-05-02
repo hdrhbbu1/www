de8c75ebd1f1e19b3296558ed4588632a834b08c
Nicholas Young <nicholas@nicholaswyoung.com>
2013-05-02 12:52:26 -0500

    tweak header.top.home

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,5 @@
 <?php snippet('head') ?>
 <header class='top<?php ecco($page->class(), " {$page->class()}") ?>'>
-	<?php snippet('header-background') ?>
 	<div class='wrap'>
 		<nav class='main'>
 			<a class='logo' href='<?php echo url() ?>'>

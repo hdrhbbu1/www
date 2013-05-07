4a840d614781931c81d5d1eedbe2417bba3f3b1f
Nicholas Young <nicholas@nicholaswyoung.com>
2013-05-07 00:20:35 -0500

    prepare to launch new article.

--- site/templates/films.php
+++ site/templates/films.php
@@ -2,13 +2,13 @@
 <section class='content<?php ecco($page->class(), " {$page->class()}") ?>'>
 	<?php foreach($page->children() as $film): ?>
 		<div class='item'>
-			<div class='feature video'>
-				<?php snippet('helpers/vimeo-embed', array('id' => $film->id())) ?>
-			</div>
-			<div class='description'>
-				<h2><?php echo $film->title() ?></h2>
-				<?php echo kirbytext($film->text()) ?>
-			</div
+		<div class='feature video'>
+			<?php snippet('helpers/vimeo-embed', array('id' => $film->id())) ?>
+		</div>
+		<div class='description'>
+			<h2><?php echo $film->title() ?></h2>
+			<?php echo kirbytext($film->text()) ?>
+		</div>
 		</div>
 	<?php endforeach ?>
 </section>

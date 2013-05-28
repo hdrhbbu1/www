58800a4314470eee7b92eb7b1a38d3ea22f7976e
Nicholas Young <nicholas@nicholaswyoung.com>
2013-05-28 09:56:39 -0500

    more pjax tweaks.

--- site/templates/photos.php
+++ site/templates/photos.php
@@ -4,7 +4,7 @@
 	<?php $images = $page->images()->not('header.jpg')->shuffle() ?>
 	<?php foreach($images as $photo): ?>
 	<div class='panel photo'>
-		<a href='<?php echo $photo->url() ?>' rel='photography' class='view'>
+		<a href='<?php echo $photo->url() ?>' rel='photography' class='view' data-skip-pjax>
 			<img src='<?php echo thumb($photo, array('width' => 500, 'height' => 500, 'crop' => true), false) ?>' />
 		</a>
 	</div>

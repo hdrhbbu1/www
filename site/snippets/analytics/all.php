dd7d5bba48887c69ac9c86128712b8aeaf2e6442
Nicholas Young <nicholas@nicholaswyoung.com>
2013-07-04 11:37:03 -0500

    add gaug.es

--- site/snippets/analytics/all.php
+++ site/snippets/analytics/all.php
@@ -1,4 +1,5 @@
 <?php if (!strstr(url(), 'site')): ?>
 	<?php snippet('analytics/google', array('id' => 'UA-33160485-1')) ?>
 	<?php snippet('analytics/gosquared', array('id' => 'GSN-283333-Y')) ?>
+	<?php snippet('analytics/gauges', array('id' => '4d628baff0d8804188000016')) ?>
 <?php endif ?>

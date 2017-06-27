6933fb3560e772422c2f7dd65ba36b12d1f1a6c3
Nicholas Young <nyoung@uptime.ventures>
2017-06-27 16:49:05 -0600

    Migrate SiteSidebar styles to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,23 +25,6 @@ export default class IndexRoute extends Component {
         <SiteSidebar
           title={title}
           description={description}
-          css={{
-            width: '30%',
-            marginRight: '5%',
-            paddingRight: '5%',
-            borderRight: '1px rgba(0, 0, 0, .1) solid',
-            '@media(max-width: 800px)': {
-              flexDirection: 'column',
-              alignItems: 'stretch',
-              borderRight: 'none',
-              width: '100%',
-              marginRight: '0',
-              paddingRight: '0',
-              marginBottom: rhythm(2),
-              paddingBottom: rhythm(2),
-              borderBottom: '1px rgba(0, 0, 0, .1) solid',
-            }
-          }}
         />
         <div
           css={{
--- src/templates/Page.js
+++ src/templates/Page.js
@@ -22,23 +22,6 @@ export default class PageTemplate extends Component {
         <SiteSidebar
           title={title}
           description={description}
-          css={{
-            width: '30%',
-            marginRight: '5%',
-            paddingRight: '5%',
-            borderRight: '1px rgba(0, 0, 0, .1) solid',
-            '@media(max-width: 800px)': {
-              flexDirection: 'column',
-              alignItems: 'stretch',
-              borderRight: 'none',
-              width: '100%',
-              marginRight: '0',
-              paddingRight: '0',
-              marginBottom: rhythm(2),
-              paddingBottom: rhythm(2),
-              borderBottom: '1px rgba(0, 0, 0, .1) solid',
-            }
-          }}
         />
         <div
           css={{

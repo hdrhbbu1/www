77183499d9ccc491c39c2db9cdad56deeccac0d5
Nicholas Young <nyoung@uptime.ventures>
2017-06-28 09:15:32 -0600

    Tune up description, flesh out Show template

--- src/templates/Show.js
+++ src/templates/Show.js
@@ -1,9 +1,64 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+
+import Container from '../components/Container'
+import SiteSidebar from '../components/SiteSidebar'
+import Main from '../components/Main'
 
 export default class ShowTemplate extends Component {
   render() {
+    const {
+      frontmatter: { title, description },
+      fields: { slug },
+      html
+    } = this.props.data.markdownRemark
+
     return (
-      <div/>
+      <Container>
+        <Helmet
+          title={title}
+          meta={[
+            {
+              name: 'description',
+              content: description,
+            }
+          ]}
+        />
+        <SiteSidebar
+          title={title}
+          description={description}
+        />
+        <Main>
+          <article>
+            <h2
+              css={{
+                marginTop: 0,
+              }}
+            >
+            </h2>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: html,
+              }}
+            />
+          </article>
+        </Main>
+      </Container>
     )
   }
 }
+
+export const pageQuery = graphql`
+  query ShowTemplate($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`

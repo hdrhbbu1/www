2dc23d15381c7ba69084f99a87677773cbbb2c22
Nicholas Young <nyoung@uptime.ventures>
2017-07-30 21:07:59 -0600

    Add post horizontal rule styles

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
+import './Post.css'
 import { rhythm } from '../util/typography'
 import EmailRegistration from '../components/EmailRegistration'
 

5087952e72bfd18ea9b3179de90a50c2af38630a
Nicholas Young <nyoung@uptime.ventures>
2017-12-22 17:07:23 -0700

    fix(substrate): Register -> Subscribe

--- src/components/SubstrateForm.js
+++ src/components/SubstrateForm.js
@@ -102,7 +102,7 @@ const SubstrateForm = () => (
               cursor: 'pointer',
             }}
           >
-            Register
+            Subscribe
           </button>
         </form>
       )

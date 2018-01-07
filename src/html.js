8e99c8525c74a5674a30c9bed396db24920d2bad
thaoamtanuyen <thaoamtanuyen@gmail.com>
2018-01-07 14:19:04 +0700

    1

--- src/html.js
+++ src/html.js
@@ -39,12 +39,12 @@ export default class HTML extends Component {
         </head>
         <body>
 			<script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
-			<ins class='adsbygoogle'
-     style='display:block; text-align:center;'
-     data-ad-layout='in-article'
-     data-ad-format='fluid'
-     data-ad-client='ca-pub-2222008371700158'
-     data-ad-slot='6074071537'></ins>
+			<ins class="adsbygoogle"
+			style="display:block; text-align:center;"
+			data-ad-layout="in-article"
+			data-ad-format="fluid"
+			data-ad-client="ca-pub-2222008371700158"
+			data-ad-slot="6074071537"></ins>
 		  { process.env.NODE_ENV === 'production' ? GoogleAdSenseSetup : null }
 
           <div
